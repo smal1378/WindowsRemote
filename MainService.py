1d5c873d4c3d87082503097913f1f657a1854804
smal1378 <smal137800@gmail.com>
2021-12-26 01:55:21 +0330

    Remove win32serviceutil for good.

--- MainService.py
+++ MainService.py
@@ -1,55 +1,32 @@
 import os
 import select
-import time
-import win32serviceutil
-import win32service
-import win32event
-import servicemanager
 import socket
 
 
-class AppServerSvc (win32serviceutil.ServiceFramework):
-    _svc_name_ = "TestService"
-    _svc_display_name_ = "Test Service"
-
-    def __init__(self, args):
-        win32serviceutil.ServiceFramework.__init__(self, args)
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-        socket.setdefaulttimeout(60)
-
-    def SvcStop(self):
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        win32event.SetEvent(self.hWaitStop)
-
-    def SvcDoRun(self):
-        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
-                              servicemanager.PYS_SERVICE_STARTED,
-                              (self._svc_name_, ''))
-        self.main()
-
-    def main(self):
-        s = socket.socket()
+class Service:
+    def __init__(self):
+        self.s = s = socket.socket()
         s.setblocking(False)
         s.bind(("", 8085))
         s.listen()
+
+    def mainloop(self):
+        s = self.s
         potential_readers = []
         buffers = []  # for each socket in potential_readers list there is a buffer here
         logged_in = []
         new_conn = []
         while True:  # mainloop
             ready_to_read, ready_to_write, in_error = select.select(potential_readers + [s],
-                                                                    potential_readers,
-                                                                    potential_readers, 60)
-            time.sleep(2)
+                                                                    new_conn, potential_readers, 2)
             for conn in in_error:
                 potential_readers.remove(conn)
                 conn.close()
             if in_error:
                 continue
-            for conn in new_conn:
-                if conn in ready_to_write:
-                    conn.sendall(b"LOGIN;")
-                    new_conn.remove(conn)
+            for conn in ready_to_write:
+                conn.sendall(b"LOGIN;")
+                new_conn.remove(conn)
             for conn in ready_to_read:
                 conn: socket.socket
                 if conn == s:
@@ -115,5 +92,5 @@ class AppServerSvc (win32serviceutil.ServiceFramework):
 
 
 if __name__ == '__main__':
-    # win32serviceutil.HandleCommandLine(AppServerSvc)
-    AppServerSvc.main(AppServerSvc)
+    service = Service()
+    service.mainloop()
